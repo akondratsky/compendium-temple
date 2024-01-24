9d2f60b594ba851aeef05b48aa8665051971a34b
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-01-24 22:27:40 +0100

    feat: beautify table

--- packages/website/src/components/ReposTable/ReposTable.tsx
+++ packages/website/src/components/ReposTable/ReposTable.tsx
@@ -1,31 +1,41 @@
-import { Space, Table } from 'antd';
+import { Table } from 'antd';
 import { observer } from 'mobx-react-lite';
 import { searchResults } from '../../store/searchResults';
 import { pagination } from '../../store/pagination';
 import { search } from '../../services/repos';
 import { columns } from './columns';
+import { useEffect } from 'react';
 
 
 export const ReposTable = observer(() => {
+  useEffect(() => {
+    search.new();
+  }, []);
+
   return (
-    <Space style={{ overflow: 'hidden' }}>
-      <Table
-        sticky={{ offsetHeader: 0, offsetSummary: 33 }}
-        dataSource={[...searchResults.repos]}
-        size='large'
-        pagination={{
-          pageSize: pagination.pageSize,
-          current: pagination.currentPage,
-          total: pagination.total,
-          onChange: (page, pageSize) => {
-            pagination.setCurrentPage(page);
-            pagination.setPageSize(pageSize);
-            search.update();
-          },
-          pageSizeOptions: [10, 25, 50, 100],
-        }}
-        columns={columns}
-      />
-    </Space>
+    <Table
+      sticky={{ offsetHeader: 0, offsetSummary: 33 }}
+      dataSource={[...searchResults.repos]}
+      size='large'
+      pagination={{
+        position: ['bottomCenter', 'bottomCenter'],
+        pageSize: pagination.pageSize,
+        current: pagination.currentPage,
+        total: pagination.total,
+        onChange: (page, pageSize) => {
+          pagination.setCurrentPage(page);
+          pagination.setPageSize(pageSize);
+          search.update();
+        },
+        pageSizeOptions: [10, 25, 50, 100],
+      }}
+      columns={columns}
+      scroll={{
+        x: true,
+        y: 'calc(100vh - 120px)',
+        scrollToFirstRowOnChange: true,
+      }}
+      style={{ height: '100%', maxHeight: '100vh' }}
+    />
   );
 });
