4ffc74f382a0ebccaf507a1e8cc7f0ecd49f8acb
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-01-28 06:59:00 +0100

    fix(website): add base url to deploy on github pages

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '/' : '/compendium-temple',
+
   cacheDir: '../../node_modules/.vite/website',
 
   envPrefix: 'WEBSITE_',
