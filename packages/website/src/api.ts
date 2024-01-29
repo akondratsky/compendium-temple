ab0e7f8c94dedb6bfd094afb3ad459d831c50bf6
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-01-29 03:30:39 +0100

    fix: change WEBSITE_API_URL

--- packages/website/src/api.ts
+++ packages/website/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // gh-pages redeployed with this homeopathic comment: |
+  // gh-pages redeployed with this homeopathic comment: ||
   baseURL: import.meta.env.WEBSITE_API_URL,
 });
